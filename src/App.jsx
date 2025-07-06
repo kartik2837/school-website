cf52b6cd61c2582d3716f3ce25bf484a13b1777d
Kartik Bhadouriya <ks8783375@gmail.com>
2025-07-06 12:28:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './innerpages/Home';
 import Abouts from './innerpages/Abouts';
 import Topbar from './component/Topbar'
@@ -29,7 +29,7 @@ function App() {
   }, []);
   return (
     <>
-      <BrowerRouter>
+    
 
       <div>
         <Topbar />
@@ -64,7 +64,7 @@ function App() {
         </a>
         <Footer />
       </div>
-        </BrowerRouter>
+       
 
 
 
